9e5b3a06d1af7443948f8506ad72033ee9e40d86
senyacherenkov <senyacherenkov@yandex.ru>
2019-06-12 19:09:54 +0300

    in progress

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <math.h>
+#include <algorithm>
 
 #define BOOST_TEST_MODULE test_main
 
@@ -8,21 +9,65 @@
 using namespace boost::unit_test;
 BOOST_AUTO_TEST_SUITE(test_suite_main)
 
-template <typename T>
+template <typename KeyType, typename ValueType>
 class AvlTree
 {
     struct Node
     {
-        Node* left {nullptr};
-        Node* right {nullptr};
-        T value;
-        int height{0};
+        AvlTree*    parent {nullptr} ;
+        AvlTree*    leftSubTree {nullptr};
+        AvlTree*    rightSubTree {nullptr};
+        ValueType   value;
+        KeyType     key;
+        int         height{1};
     };
 
+    Node* node_{nullptr};
     AvlTree() = default;
-    Node root;
+
+    int getHeight() const { return node_? node_->height: 0; }
+    int getHeightLeft() const { return node_->leftSubTree? node_->leftSubTree->getHeight():0;}
+    int getHeightRight() const { return node_->rightSubTree? node_->rightSubTree->getHeight():0;}
+
+    int getBalance() const { return node_? getHeightLeft() - getHeightRight(): 0; }
+    void recalculateHeight() { if(node_) node_->height = std::max(getHeightLeft(), getHeightRight()) + 1; }
+
+    void rotateLeft()
+    {
+        auto vertex = node_->rightSubTree;
+        node_->rightSubTree= vertex->leftSubTree;
+        vertex->leftSubTree = this;
+
+        node_->height--;
+        vertex->height++;
+    }
+
+    void rotateRight()
+    {
+        auto vertex = node_->rightSubTree;
+        node_->rightSubTree= vertex->leftSubTree;
+        vertex->leftSubTree = this;
+
+        node_->height--;
+        vertex->height++;
+    }
+
+    void bigRotateLeft()
+    {
+        rotateRight(node->right);
+        rotateLeft(node);
+    }
+
+    void bigRotateRight(Node* node)
+    {
+        rotateLeft(node->left);
+        rotateRight(node);
+    }
+
 
 public:
+
+
     void insert(T value);
     T remove(int index);
     void rebalance();
